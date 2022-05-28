ae95bdc929c2bc8376a71dada72daf831f85fe7a
Ramazan Küçükkoç <ramazankucukkoc43@gmail.com>
2022-05-28 22:05:30 +0300

    Add project files.

---
+++ ThreadOrnegi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadOrnegi
+{
+
+    class Program
+    {
+        public class ThreadIslemi
+        {
+            public void ThreadOrnegi()
+            {
+                Thread.Sleep(5000);  
+                Console.WriteLine(" 5 saniye Bekleme Sonrası Thread Örneği");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ThreadIslemi threadIslemi = new ThreadIslemi();
+            Console.WriteLine("Ramazan KÜÇÜKKOÇ ");
+            Console.WriteLine("NO : 21807052 "); 
+            Console.WriteLine("----------------------");
+            threadIslemi.ThreadOrnegi();
+          
+            Console.ReadLine();
+        }
+    }
+}
